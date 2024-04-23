f30ad88b3600f3ee767b84dbb732e62d4f9d1f98
branGitFox <vixfgit@gmail.com>
2024-04-23 16:49:03 +0300

    stylisation du components header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,17 +5,41 @@ const style = {
         alignItems:'center',
         justifyContent:'space-between',
         padding:'20px',
-        borderBottom:'solid 1px black'
+        // borderBottom:'solid 1px black',
+        backgroundColor:'rgb(2, 10, 121)',
+        boxShadow:'0px 1px 10px',
+        height:'12vh'
+    },
+
+    span:{
+        color:'orange',
+        fontWeight:'100',
+        fontFamily:'Segoe UI, Tahoma, Geneva, Verdana, sans-serif'
+
+    },
+
+    h2:{
+        fontWeight:'700',
+        color:'white'
+    },
+    navlinka:{
+        padding:'20px 30px',
+        textDecoration:'none',
+        color:'whitesmoke',
+        backgroundColor:'rgb(101, 0, 148)',
+        borderRadius:'5px'
     }
+
+    
 }
 
 function Header(){
     return <nav style={style.nav}>
         <div className="nav-logo">
-            <h2><span>{'<'}</span>BrandonDEV<span>{'/>'}</span></h2>
+            <h2 style={style.h2}><span style={style.span}>{'<'}</span>BrandonDEV<span style={style.span}>{'/>'}</span></h2>
         </div>
         <div className="nav-link">
-            <a href="#">Github</a>
+            <a style={style.navlinka} href="#">Github</a>
         </div>
     </nav>
 }
