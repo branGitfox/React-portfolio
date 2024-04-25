9c9c80381499b4ef07b5efe7882f90fd0ce79466
branGitFox <vixfgit@gmail.com>
2024-04-25 13:40:26 +0300

    stylisation du conteneur des reseaux sociaux terminée

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './Components/Header/Header'
 import Type from './Components/Type/Type'
 import Body from './Components/Body/Body'
 import Download from './Components/Cv/Download'
+import Social from './Components/Social/Social'
 
 function App() {  
 
@@ -12,6 +13,7 @@ function App() {
         <Type />
         <Body />
         <Download />
+        <Social />
     </div>
   </>
 }
--- src/Components/Social/Social.jsx
+++ src/Components/Social/Social.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
-function Social(){
-    return <div className="social-container">
+import  './Social.css'
+const style = {
+    div:{
+        display:'flex',
+        justifyContent:'center',
+        // border:'solid 1px black',
+        margin:'auto',
+        marginTop:'7rem',
+        gap:'4rem'
+
+    },
+
+    childDiv:{
+        width:'50px',
+        height:'50px',
+        borderRadius:'50%',
+        border:'solid 2px purple',
+        // marginRight:'10px',
         
+    }
+
+}
+function Social(){
+    return <div style={style.div} className="social-container">
+        <div style={style.childDiv} className="youtube"></div>
+        <div style={style.childDiv} className="mails"></div>
+        <div style={style.childDiv} className="instagram"></div>
+        <div style={style.childDiv} className="facebook"></div>
     </div>
 }
 
